8a482684a42ae107a74b6536949f87d6f111807a
Seputaes <seputaes@live.com>
2018-10-06 02:36:13 -0700

    [EmbedBuilder] Add EmbedBuilder cog, for building simple Embeds
    * Current functionality is extremely limited. More coming.

--- embedbuilder/embedbuilder.py
+++ embedbuilder/embedbuilder.py
@@ -23,13 +23,50 @@ class EmbedBuilder(BaseSepCog):
     @commands.guild_only()
     @checks.mod_or_permissions(manage_channels=True)
     async def _embed(self, ctx: Context):
+        """
+        This is the help
+        """
+        self.logger.error('embed group')
         await ctx.send_help()
 
-
-    @_embed.commands(name="create")
+    @_embed.command(name="basic")
     @commands.guild_only()
     @checks.mod_or_permissions(manage_channels=True)
-    async def _embed(self, ctx: Context, msg: str):
-        message = ctx.message  # type: discord.Message
+    async def _embed_basic(self, ctx: Context, *, msg: str):
+        """
+        Creates a basic embed with only a description/color, posted in the specified channel. See full command help for detailed message format.
 
-        var = True
+
+        # StartDescription
+        This is the embed's description text.
+        It can have multiple lines and contain **markdown** and `code blocks``
+
+        It can even contain
+
+        ```
+        Multiline Code
+        Blocks
+        ```
+
+        Here's one last line to show that line breaks matter.
+        # EndDescription
+
+        # StartColor
+        0x0000ff
+        # EndColor
+
+        # StartChannel
+        01234567890
+        # EndChannel
+        """
+        message_text = ctx.message.content
+
+        channel_id = message_text.split('# StartChannel\n')[1].split('\n# EndChannel')[0]
+        channel = discord.utils.get(ctx.guild.channels, id=int(channel_id))
+
+        description = message_text.split('# StartDescription\n')[1].split('\n# EndDescription')[0]
+        color_str = message_text.split('# StartColor\n')[1].split('\n# EndColor')[0]
+
+        embed = discord.Embed(description=description, color=int(color_str, 0))
+        await channel.send(embed=embed)
+        pass
